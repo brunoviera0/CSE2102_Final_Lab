41cba3a4f1fa8ba398631bf30876548ddee5ebc4
unknown <bruno.viera@uconn.edu>
2023-12-08 16:34:29 -0500

    working

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
 test('renders the welcome message', () => {
   render(<App />);
-  const welcomeElement = screen.getByText(/Welcome to the final CSE2102 Quiz!/i);
+  const welcomeElement = screen.getByText(/The Final CSE2102 Quiz!/i);
   expect(welcomeElement).toBeInTheDocument();
 });
--- src/tests/Score.test.js
+++ src/tests/Score.test.js
@@ -3,18 +3,24 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Score from '../components/Score';
 
-test('renders the correct score and user name in a table', () => {
+test('renders the correct score, user name, and score history', () => {
   const userName = 'Bruno';
   const score = 5;
   const totalQuestions = 10;
+  const scoreHistory = [3, 4, 5, 6, 7];
 
-  render(<Score score={score} totalQuestions={totalQuestions} userName={userName} />);
+  render(<Score score={score} totalQuestions={totalQuestions} userName={userName} scoreHistory={scoreHistory} />);
 
-  const userCell = screen.getByText('User:');
-  const userNameCell = userCell.nextSibling;
-  const scoreCell = screen.getByText('Score:');
-  const scoreValueCell = scoreCell.nextSibling;
+  const userNameHeader = screen.getByText(`Results for: ${userName}`);
+  const scoreText = screen.getByText(`You scored a ${score} out of ${totalQuestions}, congrats!`);
+  const scoreHistoryHeader = screen.getByText('Your score history for this session (in chronological order) starting with the current attempt:');
+  
+  expect(userNameHeader).toBeInTheDocument();
+  expect(scoreText).toBeInTheDocument();
+  expect(scoreHistoryHeader).toBeInTheDocument();
 
-  expect(userNameCell.textContent).toBe(userName);
-  expect(scoreValueCell.textContent).toBe(`${score} out of ${totalQuestions}`);
+  scoreHistory.forEach((pastScore, index) => {
+    const scoreHistoryItem = screen.getByText(`Score: ${pastScore}/${totalQuestions}`);
+    expect(scoreHistoryItem).toBeInTheDocument();
+  });
 });
